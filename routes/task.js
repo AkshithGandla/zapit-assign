7c3a1883e5b93a4c4b523e69fc7d27d2c276bc70
AkshithGandla <akshithgandla7891@gmail.com>
2023-03-10 02:58:28 +0530

    add required routes

--- routes/task.js
+++ routes/task.js
@@ -3,5 +3,9 @@ const taskController = require("../controllers/taskController.js");
 const router = express.Router();
 
 router.post("/", taskController.create_task);
+router.get("/", taskController.get_all_tasks);
+router.get("/:taskId", taskController.get_task);
+router.patch("/:taskId", taskController.update_task);
+router.delete("/:taskId", taskController.delete_task);
 
 module.exports = router;
