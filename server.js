33872fe7224275f67d04301ff769ba186937d802
AkshithGandla <akshithgandla7891@gmail.com>
2023-03-09 15:53:58 +0530

    setup folder structure

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from "express";
-
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 //configuration
 const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
