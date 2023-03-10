958306e2959c75d40140821fa696c2a22936bcec
AkshithGandla <akshithgandla7891@gmail.com>
2023-03-10 19:09:16 +0530

    add auth checks to logout route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/userController.js");
+const { checkAuth } = require("../middleware/checkAuth.js");
 const router = express.Router();
 
 router.post("/signUp", userController.signUp);
 router.post("/login", userController.login);
-router.get("/logout", userController.logout);
+router.get("/logout", checkAuth, userController.logout);
 
 module.exports = router;
