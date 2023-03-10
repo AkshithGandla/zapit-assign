5e53dc8ae381fb7c14ce8baafecc7e264d72da1a
AkshithGandla <akshithgandla7891@gmail.com>
2023-03-10 17:21:58 +0530

    create user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+      type: String,
+      required: true,
+      min: 4,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 4,
+      max: 20,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 20,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", TaskSchema);
+module.exports = User;
