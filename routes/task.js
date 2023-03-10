8610157679956d0f97e2e926b729975fbe5002e2
AkshithGandla <akshithgandla7891@gmail.com>
2023-03-10 19:42:25 +0530

    add authorization to task routes

--- routes/task.js
+++ routes/task.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const taskController = require("../controllers/taskController.js");
+const { checkAuth } = require("../middleware/checkAuth.js");
 const router = express.Router();
 
-router.post("/", taskController.create_task);
-router.get("/", taskController.get_all_tasks);
-router.get("/:taskId", taskController.get_task);
-router.patch("/:taskId", taskController.update_task);
-router.delete("/:taskId", taskController.delete_task);
+router.post("/", checkAuth, taskController.create_task);
+router.get("/", checkAuth, taskController.get_all_tasks);
+router.get("/:taskId", checkAuth, taskController.get_task);
+router.patch("/:taskId", checkAuth, taskController.update_task);
+router.delete("/:taskId", checkAuth, taskController.delete_task);
 
 module.exports = router;
